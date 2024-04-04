6fca77cb1fe90987b221e0b62fccfccba5bf02cf
oleno <es.moname@gmail.com>
2024-04-04 08:42:47 +0200

    Implement theme management with Redux slice

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { links } from "@/utils";
+import { NavLink } from "react-router-dom";
 
 export const NavLinks = () => {
   return (
-    <div>NavLinks</div>
+    <div className='lg:flex justify-center items-center gap-x-4'>
+      {links.map((link) => {
+        return (
+        <NavLink
+          to={link.href}
+          key={link.label}
+          className={({ isActive }) => {
+            return `capitalize font-light tracking-wide ${
+              isActive ? 'text-primary' : ''
+            }`;
+          }}
+          >
+          {link.label}
+        </NavLink>
+        )
+      })}
+    </div>
   )
 }
