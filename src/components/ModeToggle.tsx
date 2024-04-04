42b6b2ce525ca14680c9e08319006f3d9de9135c
oleno <es.moname@gmail.com>
2024-04-04 09:48:06 +0200

    Implement theme toggle functionality with sun and moon icons

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { Moon, Sun } from "lucide-react";
+import { useAppDispatch } from "@/hooks";
+import { Button} from "./ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { setTheme } from "@/features/theme/themeSlice";
 
 export const ModeToggle = () => {
+  const dispatch = useAppDispatch();
+
   return (
-    <div>ModeToggle</div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button size='icon' variant='outline'>
+        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0"/>
+        <Moon className="absolute h-[1.2rem] w-[1.2rem rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
+        <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => dispatch(setTheme('light'))}>
+          Light</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => dispatch(setTheme('dark'))}>
+          Dark</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => dispatch(setTheme('system'))}>
+          System</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 export const NavLinks = () => {
   return (
-    <div className='lg:flex justify-center items-center gap-x-4'>
+    <div className='hidden lg:flex justify-center items-center gap-x-4'>
       {links.map((link) => {
         return (
         <NavLink
