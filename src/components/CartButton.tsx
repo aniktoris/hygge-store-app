fb0f0432f5b81a600e821da108b840528ee71a0a
oleno <es.moname@gmail.com>
2024-04-04 10:31:56 +0200

    Implement CartButton component with shopping cart icon and item count badge

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { Button } from "./ui/button";
+import { ShoppingCart } from "lucide-react";
+import { Link } from "react-router-dom";
 
 export const CartButton = () => {
+  const numItemsInCart = 5;
+
   return (
-    <div>CartButton</div>
+    <Button asChild size='icon' variant='outline' 
+    className="flex justify-center items-center relative">
+      <Link to='/cart'>
+      <ShoppingCart/>
+      <span className="absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs">{numItemsInCart}</span>
+      </Link>
+    </Button>
   )
 }
