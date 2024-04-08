bd04cd0d7fa24a758aac9f46c70db860d76759cf
oleno <es.moname@gmail.com>
2024-04-08 14:32:23 +0200

    implement products grid

--- src/components/ProductsGrid.tsx
+++ src/components/ProductsGrid.tsx
@@ -1,6 +1,36 @@
+import { formatAsDollars } from "@/utils/formatAsDollars";
+import { ProductsResponse } from "@/utils/types";
+import { useLoaderData } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Card, CardContent } from "./ui/card";
 
 export const ProductsGrid = () => {
+  const {data: products} = useLoaderData() as ProductsResponse;
+  
   return (
-    <div>ProductsGrid</div>
+    <div>
+      {products.map((product) => {
+        const {title, price, image} = product.attributes;
+        const dollarsAmount = formatAsDollars({price: price});
+
+        return (
+          <Link to={`/products/${product.id}`} key={product.id}>
+            <Card>
+              <CardContent className='p-4'>
+            <img
+            src={image}
+            alt={title}
+            className="rounded-md h-64 md:h-48 w-full object-cover"
+            />
+            <div className="mt-4 text-center">
+              <h2 className="text-xl font-semibold capitalize">{title}</h2>
+              <p className="text-primary font-light mt-2">{dollarsAmount}</p>
+            </div>
+            </CardContent>
+            </Card>
+          </Link>
+        )
+      })
+      }</div>
   )
 }
