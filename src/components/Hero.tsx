cc1ef975b836a949e26cd954bd074ff8c64767a0
oleno <es.moname@gmail.com>
2024-04-09 09:52:22 +0200

    add hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,24 @@
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+import { HeroCarousel } from "./HeroCarousel";
 
 export const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+      <div>
+        <h1 className="max-w-2x text-4xl font-bold tracking-tight sm:text-6xl">
+          We're changing the way people shop.
+        </h1>
+        <p className="mt-8 max-w-xl text-lg leading-8">
+        Our innovative approach to online shopping is revolutionizing the way people discover and purchase products. With our curated selection and seamless user experience, finding what you need has never been easier. Join us in shaping the future of retail with Hygge!
+        </p>
+        <Button asChild size='lg' className="mt-10">
+          <Link to='/products'>
+            Our Products
+          </Link>
+        </Button>
+      </div>
+      <HeroCarousel/>
+    </section>
   )
 }
